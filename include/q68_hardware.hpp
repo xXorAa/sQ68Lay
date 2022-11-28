9a9e1f33df447e40c86733020bcf2ae00a0750bc
Graeme Gregory <graeme@xora.org.uk>
2022-11-28 17:11:49 +0000

    q68_hardware : add RTC and hires timer
    Signed-off-by: Graeme Gregory <graeme@xora.org.uk>

--- include/q68_hardware.hpp
+++ include/q68_hardware.hpp
@@ -14,9 +14,14 @@ extern uint8_t q68_pc_intr;
 extern uint8_t q68_kbd_status;
 extern uint8_t q68_q68_dmode;
 
+// time offset for RTC
+constexpr uint32_t qdos_time = (9*365+2)*86400;
+
 // keyboard lock and queue
 extern SDL_SpinLock q68_kbd_lock;
 extern std::queue<uint8_t> q68_kbd_queue;
+
+constexpr uint32_t pc_clock     = 0x18000;
 
 constexpr uint32_t pc_tctrl     = 0x18002;
 
@@ -26,9 +31,13 @@ constexpr uint8_t pc_intrf      = 0x08;
 constexpr uint32_t mc_stat      = 0x18063;
 constexpr uint8_t mc_stat_mode  = 1 << 3;
 
+constexpr uint32_t q68_timer    = 0x1c060;
+
 constexpr uint32_t kbd_code     = 0x1c140;
+
 constexpr uint32_t kbd_unlock   = 0x1c144;
 constexpr uint8_t kbd_ackn      = 1 << 0;
+
 constexpr uint32_t kbd_status   = 0x1c148;
 constexpr uint8_t kbd_rcv       = 1 << 0;
 constexpr uint8_t kbd_isint     = 1 << 7;
--- src/q68_hardware.cpp
+++ src/q68_hardware.cpp
@@ -4,6 +4,8 @@
  * SPDX: GPL-2.0-only
  */
 
+#include <chrono>
+#include <ctime>
 #include <iostream>
 #include <queue>
 #include <SDL.h>
@@ -27,14 +29,47 @@ std::queue<uint8_t> q68_kbd_queue;
 
 static uint8_t keys[3] = {0x1c, 0xF0, 0x1C};
 
+static uint32_t q68_update_time()
+{
+    const auto clk = std::chrono::system_clock::now();
+    return std::chrono::duration_cast<std::chrono::seconds>(
+                   clk.time_since_epoch()).count() + qdos_time;
+}
+
+#ifdef Q68_EMU
+static uint32_t q68_update_hires()
+{
+    uint64_t freq = SDL_GetPerformanceFrequency();
+
+    return SDL_GetPerformanceCounter() / (freq / 40000000);
+}
+#endif
+
 unsigned int q68_read_hw_8(unsigned int addr)
 {
     int kcode = 0;
 
     //std::cout << "HWR8: " << std::hex << addr << std::endl;
     switch (addr) {
+        case pc_clock:
+            return q68_update_time() >> 24;
+        case pc_clock + 1:
+            return (q68_update_time() >> 16) & 0xFF;
+        case pc_clock + 2:
+            return (q68_update_time() >> 8) & 0xFF;
+        case pc_clock + 3:
+            return q68_update_time() & 0xFF;
         case pc_intr:
             return q68_pc_intr;
+#ifdef Q68_EMU
+        case q68_timer:
+            return q68_update_hires() >> 24;
+        case q68_timer + 1:
+            return (q68_update_hires() >> 16) & 0xFF;
+        case q68_timer + 2:
+            return (q68_update_hires() >> 8) & 0xFF;
+        case q68_timer + 3:
+            return q68_update_hires() & 0xFF;
         case kbd_code:
             SDL_AtomicLock(&q68_kbd_lock);
             if (q68_kbd_queue.size()) {
@@ -46,6 +81,7 @@ unsigned int q68_read_hw_8(unsigned int addr)
             return q68_kbd_status;
         case q68_dmode:
             return q68_q68_dmode;
+#endif
         default:
             break;
     }
@@ -57,13 +93,37 @@ unsigned int q68_read_hw_16(unsigned int addr)
 {
     //std::cout << "HWR16: " << std::hex << addr << std::endl;
 
+    switch(addr) {
+        case pc_clock:
+            return q68_update_time() >> 16;
+        case pc_clock + 2:
+            return q68_update_time() & 0xFFFF;
+#ifdef Q68_EMU
+        case q68_timer:
+            return q68_update_hires() >> 16;
+        case q68_timer + 2:
+            return q68_update_hires() & 0xFFFF;
+#endif
+        default:
+            break;
+    }
     return 0;
 }
 
 unsigned int q68_read_hw_32(unsigned int addr)
 {
     //std::cout << "HWR32: " << std::hex << addr << std::endl;
-
+    switch (addr) {
+        case pc_clock:
+            return q68_update_time();
+            break;
+#ifdef Q68_EMU
+        case q68_timer:
+            return q68_update_hires();
+#endif
+        default:
+            break;
+    }
     return 0;
 }
 
@@ -76,7 +136,9 @@ void q68_write_hw_8(unsigned int addr, unsigned int val)
             return;
         case mc_stat:
             q68_mc_stat = val;
+            q68ScreenChangeMode(val >> 3);
             return;
+#ifdef Q68_EMU
         case kbd_code:
             return;
         case kbd_unlock:
@@ -97,6 +159,7 @@ void q68_write_hw_8(unsigned int addr, unsigned int val)
             q68_q68_dmode = val;
             q68ScreenChangeMode(val & 7);
             return;
+#endif
         default:
             break;
     }
