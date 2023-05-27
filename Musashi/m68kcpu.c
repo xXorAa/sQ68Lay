9fedb43de397de1b14fd4eaf8ed79388b3b6ffc7
Graeme Gregory <graeme@xora.org.uk>
2023-05-27 01:00:57 +0100

    Musashi/m68kcpu.c : change to 68000 CPU
    68010 was a c&p error, and it turns out smsqe run on 68010 but Minerva
    does not. Ends in an infinite loop in the 020 detection code.
    
    Signed-off-by: Graeme Gregory <graeme@xora.org.uk>

--- Musashi/m68kcpu.c
+++ Musashi/m68kcpu.c
@@ -799,7 +799,7 @@ void m68k_set_cpu_type(unsigned int cpu_type)
 			HAS_PMMU	 = 0;
 			return;
 		case M68K_CPU_TYPE_68000_TG68K:
-			m68k_set_cpu_type(M68K_CPU_TYPE_68010);
+			m68k_set_cpu_type(M68K_CPU_TYPE_68000);
 			CPU_ADDRESS_MASK = 0xffffffff;
 			return;
 		case M68K_CPU_TYPE_SCC68070:
