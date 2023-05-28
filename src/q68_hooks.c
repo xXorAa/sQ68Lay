ae8afea00e3fa970ed2dcd90f187dc37e698fe88
Graeme Gregory <graeme@xora.org.uk>
2023-05-28 02:00:13 +0100

    q68_hooks.c : increase address range shown
    Signed-off-by: Graeme Gregory <graeme@xora.org.uk>

--- src/q68_hooks.c
+++ src/q68_hooks.c
@@ -11,6 +11,6 @@ void emu_hook_pc(unsigned int pc)
 	if (trace) {
 		char disBuf[256];
 		m68k_disassemble(disBuf, pc, M68K_CPU_TYPE_68000);
-		printf("%04X %s\n", pc, disBuf);
+		printf("%08X %s\n", pc, disBuf);
 	}
 }
