7d706b3d81d4027491c28956e5b4b9e762afab66
Graeme Gregory <graeme@xora.org.uk>
2023-05-30 19:18:34 +0100

    q68_sd.c : switch to glib for file io and do multi block read
    Using glib and mempory mapped files.
    
    Signed-off-by: Graeme Gregory <graeme@xora.org.uk>

--- src/q68_sd.c
+++ src/q68_sd.c
@@ -4,13 +4,13 @@
  * SPDX: GPL-2.0-only
  */
 
-#include <fcntl.h>
+#include <glib.h>
 #include <SDL.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 #include "emulator_options.h"
 #include "m68k.h"
@@ -19,19 +19,28 @@
 #define O_BINARY 0
 #endif
 
-int sd1File = -1;
+GMappedFile *sd1File = NULL;
 uint8_t sd1CmdBuf[8];
 int sd1CmdIdx = 0;
-uint8_t sd1RespBuf[2 + 512 +2];
+uint8_t sd1Resp = 0;
+uint8_t sd1RespBuf[1 + 512 + 2];
+bool sd1Multi = false;
+int sd1Block;
 int sd1RespIdx = 0;
-int sd2File = -1;
+
+GMappedFile * sd2File = NULL;
 
 void q68InitSD(void)
 {
 	const char *sd1Filename = emulatorOptionString("sd1");
 	if (strlen(sd1Filename) > 0) {
 		printf("SD1: %s\n", sd1Filename);
-		sd1File = open(sd1Filename, O_RDWR | O_BINARY);
+
+		GError *err = NULL;
+		sd1File = g_mapped_file_new(sd1Filename, true, &err);
+		if (err != NULL) {
+			fprintf(stderr, "Error: opening SD1 %s\n", err->message);
+		}
 		sd1CmdIdx = 0;
 	}
 }
@@ -50,22 +59,54 @@ void q68ProcessSDCmd(__attribute__ ((unused))int sd, uint8_t cmdByte)
 
 		sd1CmdIdx = 0;
 
-		sd1RespIdx = 0;
-		sd1RespBuf[0] = 0x00;
-		sd1RespBuf[1] = 0xFE;
+		sd1RespIdx = -1;
+		sd1Resp = 0x00;
+		sd1RespBuf[0] = 0xFE;
 
 		uint32_t block = SDL_SwapBE32(*(uint32_t *)&sd1CmdBuf[2]);
 
-		lseek(sd1File, block * 512, SEEK_SET);
-		read(sd1File, &sd1RespBuf[2], 512);
+		memcpy(&sd1RespBuf[1], g_mapped_file_get_contents(sd1File) + (block * 512), 512);
+
+		sd1Multi = false;
+	}
+
+	if ((sd1CmdBuf[0] = 0xFF) &&
+		(sd1CmdBuf[1] = 0x52) &&
+		(sd1CmdBuf[6] = 0xFF)) {
+
+		sd1CmdIdx = 0;
+
+		sd1RespIdx = -1;
+		sd1Resp = 0x00;
+		sd1RespBuf[0] = 0xFE;
+
+		uint32_t block = SDL_SwapBE32(*(uint32_t *)&sd1CmdBuf[2]);
+
+		memcpy(&sd1RespBuf[1], g_mapped_file_get_contents(sd1File) + (block * 512), 512);
+
+		sd1Block = block + 1;
+
+		sd1Multi = true;
 	}
 }
 
 uint8_t q68ProcessSDResponse(__attribute__ ((unused))int sd)
 {
-	if (sd1RespIdx < 516) {
+	if (sd1RespIdx < 0) {
+		sd1RespIdx = 0;
+		return sd1Resp;
+	}
+
+	if (sd1RespIdx < 515) {
 		return sd1RespBuf[sd1RespIdx++];
 	}
 
+	if (sd1Multi) {
+		sd1RespBuf[0] = 0xFE;
+		memcpy(&sd1RespBuf[1], g_mapped_file_get_contents(sd1File) + (sd1Block * 512), 512);
+		sd1Block++;
+		sd1RespIdx = 0;
+		return sd1RespBuf[sd1RespIdx++];
+	}
 	return 0;
 }
