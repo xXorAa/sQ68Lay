58651f3df64272724bf23de98e99e140f8e22c66
Graeme Gregory <graeme@xora.org.uk>
2024-02-04 15:17:31 +0000

    qlay_hooks.c : add status register handling to trace
    Signed-off-by: Graeme Gregory <graeme@xora.org.uk>

--- src/qlay_hooks.c
+++ src/qlay_hooks.c
@@ -14,6 +14,8 @@ void emu_hook_pc(unsigned int pc)
 		char disBuf[256];
 		uint32_t d[8];
 		uint32_t a[8];
+		uint32_t sr;
+
 		int i;
 
 		for (i = 0; i < 8; i++) {
@@ -24,12 +26,15 @@ void emu_hook_pc(unsigned int pc)
 			a[i] = m68k_get_reg(NULL, M68K_REG_A0 + i);
 		}
 
+		sr = m68k_get_reg(NULL, M68K_REG_SR) & 0x1F;
+
 		m68k_disassemble(disBuf, pc, M68K_CPU_TYPE_68000);
 
 		// Output the registers and assembly
 		printf("  | %8c| %8c| %8c| %8c| %8c| %8c| %8c| %8c|\n", '0', '1', '2', '3', '4', '5', '6', '7');
 		printf("D | %8x| %8x| %8x| %8x| %8x| %8x| %8x| %8x|\n", d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7]);
 		printf("A | %8x| %8x| %8x| %8x| %8x| %8x| %8x| %8x|\n", a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]);
+		printf("S | X=%1d N=%1d Z=%1d V=%1d C=%1d\n", (sr >> 4) & 1, (sr >> 3) & 1, (sr >> 2) & 1, (sr >> 1) & 1, sr & 1);
 		printf("\n");	
 		printf("%04X %s\n\n", pc, disBuf);
 	}
