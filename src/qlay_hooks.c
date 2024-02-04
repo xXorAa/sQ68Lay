7321fe860f4d62d30d605ced7a6a68cfbe471378
Graeme Gregory <graeme@xora.org.uk>
2024-02-04 14:19:33 +0000

    qlay_hooks.c : improve the tracing support
    Add registers before the instructions disassembly!
    
    Signed-off-by: Graeme Gregory <graeme@xora.org.uk>

--- src/qlay_hooks.c
+++ src/qlay_hooks.c
@@ -12,7 +12,25 @@ void emu_hook_pc(unsigned int pc)
 {
 	if (trace) {
 		char disBuf[256];
+		uint32_t d[8];
+		uint32_t a[8];
+		int i;
+
+		for (i = 0; i < 8; i++) {
+			d[i] = m68k_get_reg(NULL, M68K_REG_D0 + i);
+		}
+
+		for (i = 0; i < 8; i++) {
+			a[i] = m68k_get_reg(NULL, M68K_REG_A0 + i);
+		}
+
 		m68k_disassemble(disBuf, pc, M68K_CPU_TYPE_68000);
-		printf("%04X %s\n", pc, disBuf);
+
+		// Output the registers and assembly
+		printf("  | %8c| %8c| %8c| %8c| %8c| %8c| %8c| %8c|\n", '0', '1', '2', '3', '4', '5', '6', '7');
+		printf("D | %8x| %8x| %8x| %8x| %8x| %8x| %8x| %8x|\n", d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7]);
+		printf("A | %8x| %8x| %8x| %8x| %8x| %8x| %8x| %8x|\n", a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]);
+		printf("\n");	
+		printf("%04X %s\n\n", pc, disBuf);
 	}
 }
