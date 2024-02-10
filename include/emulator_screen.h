a4f10f8de7af5247e98b8827facbc2059f6d1de6
Graeme Gregory <graeme@xora.org.uk>
2024-02-10 19:22:03 +0000

    emulator_screen.c : handle second QL screen
    Signed-off-by: Graeme Gregory <graeme@xora.org.uk>

--- include/emulator_screen.h
+++ include/emulator_screen.h
@@ -3,9 +3,13 @@
 #ifndef EMULATOR_SCREEN_H
 #define EMULATOR_SCREEN_H
 
+#include <stdbool.h>
+
 void emulatorScreenInit(int screenMode);
 void emulatorUpdatePixelBuffer(void);
 void emulatorRenderScreen(void);
 void emulatorScreenChangeMode(int qlMode);
+
+extern bool emulatorSecondScreen;
 
 #endif /* EMULATOR_SCREEN_H */
