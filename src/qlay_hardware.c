1192a00f6885d674d120d61ef8213cc2cbf26ba0
Graeme Gregory <graeme@xora.org.uk>
2024-02-13 20:46:59 +0000

    qlay_io.c : reorder registers into address order
    Signed-off-by: Graeme Gregory <graeme@xora.org.uk>

--- src/qlay_hardware.c
+++ src/qlay_hardware.c
@@ -31,7 +31,7 @@ static uint32_t qlayUpdateTime(void)
 uint8_t qlHardwareRead8(unsigned int addr)
 {
 	switch (addr) {
-	case PC_CLOCK:
+	case PC_CLOCK: // 18000
 		return qlayUpdateTime() >> 24;
 	case PC_CLOCK + 1:
 		return (qlayUpdateTime() >> 16) & 0xFF;
@@ -39,12 +39,15 @@ uint8_t qlHardwareRead8(unsigned int addr)
 		return (qlayUpdateTime() >> 8) & 0xFF;
 	case PC_CLOCK + 3:
 		return qlayUpdateTime() & 0xFF;
-	case PC_IPCRD:
-	case PC_TRAK1:
-	case PC_TRAK2:
+	case PC_IPCRD: // 18020
 		return readQLHw(addr);
-	case PC_INTR:
+	case PC_INTR: // 18021
 		return EMU_PC_INTR;
+	case PC_TRAK1: // 18022
+	case PC_TRAK2: // 18023
+		return readQLHw(addr);
+	case MC_STAT: // 18063
+		return EMU_MC_STAT;
 	default:
 		break;
 	}
@@ -56,23 +59,27 @@ void qlHardwareWrite8(unsigned int addr, uint8_t val)
 {
 	//std::cout << "HWW8: " << std::hex << addr << "," << val << std::endl;
 	switch (addr) {
-	case PC_TCTRL:
+	case PC_TCTRL: // 18002
 		wrZX8302(val);
 		return;
-	case PC_IPCWR:
+	case PC_IPCWR: // 18003
 		wr8049(val);
 		return;
-	case PC_MCTRL:
+	case PC_MCTRL: // 18020
 		wrmdvcntl(val);
 		return;
-	case PC_INTR: {
+	case PC_INTR: // 18021
+	{
 		uint8_t tmp;
 		EMU_PC_INTR_MASK = (val & 0xE0);
 		tmp = val & 0x1F;
 		EMU_PC_INTR &= ~tmp;
 		return;
 	}
-	case MC_STAT:
+	case PC_TRAK1: // 18022
+		writeMdvSer(val);
+		return;
+	case MC_STAT: // 18063
 		EMU_MC_STAT = val;
 		if (val & MC__SCRN) {
 			emulatorSecondScreen = true;
@@ -84,9 +91,6 @@ void qlHardwareWrite8(unsigned int addr, uint8_t val)
 		} else {
 			emulatorScreenChangeMode(1);
 		}
-		return;
-	case PC_TRAK1:
-		writeMdvSer(val);
 		return;
 	default:
 		break;
