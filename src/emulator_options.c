337fd568ff77619611b40cd2192cd20643ff1ec2
Graeme Gregory <graeme@xora.org.uk>
2025-02-03 16:14:55 +0000

    emulator_options.c : add loglevel argument
    This allows the user to change the current logging level of the
    emulator. Making it more or less verbose in usage!

--- src/emulator_options.c
+++ src/emulator_options.c
@@ -17,13 +17,13 @@
 #pragma GCC diagnostic pop
 
 #include "ini.h"
+#include "log.h"
 #include "utlist.h"
 #include "utstring.h"
 #include "version.h"
 
-
 #ifdef Q68_EMU
-static const char * const helpTextInit = "\n\
+static const char *const helpTextInit = "\n\
 Usage: sq68ux [OPTIONS] [args...]\n\
 \n\
 Positionals:\n\
@@ -35,7 +35,7 @@ Options:\n\
 #endif
 
 #ifdef QLAY_EMU
-static const char * const helpTextInit = "\n\
+static const char *const helpTextInit = "\n\
 Usage: sqlay3 [OPTIONS] [args...]\n\
 \n\
 Positionals:\n\
@@ -46,9 +46,8 @@ Options:\n\
   -f,--CONFIG [sqlay3.ini]    Read an ini file\n";
 #endif
 
-static const char * const helpTextTail = "\
+static const char *const helpTextTail = "\
   --version                   version number\n";
-
 
 enum emuOptsType {
 	EMU_OPT_INT,
@@ -74,72 +73,108 @@ struct emuOpts {
 struct emuOpts emuOptions[] = {
 #ifdef SQLUX_EMU
 
-{"bdi1", "", "file exposed by the BDI interface", EMU_OPT_CHAR, 0 , NULL},
-{"boot_cmd", "b", "command to run on boot (executed in basic)", EMU_OPT_CHAR, 0, NULL},
-{"boot_device", "d", "device to load BOOT file from", EMU_OPT_CHAR, 0, "mdv1"},
-{"cpu_hog", "", "1 = use all cpu, 0 = sleep when idle", EMU_OPT_INT, 1, NULL},
-{"device", "", "QDOS_name,path,flags (may be used multiple times", EMU_OPT_DEV, 0, NULL},
-{"fast_startup", "", "1 = skip ram test (does not affect Minerva)", EMU_OPT_INT, 0, NULL},
-{"filter", "", "enable bilinear filter when zooming", EMU_OPT_INT, 0, NULL},
-{"fixaspect", "", "0 = 1:1 pixel mapping, 1 = 2:3 non square pixels, 2 = BBQL aspect non square pixels", EMU_OPT_INT, 0, NULL},
-{"iorom1", "", "rom in 1st IO area (Minerva only 0x10000 address)", EMU_OPT_CHAR, 0, NULL},
-{"iorom2", "", "rom in 2nd IO area (Minerva only 0x14000 address)", EMU_OPT_CHAR, 0, NULL},
-{"joy1", "", "1-8 SDL2 joystick index", EMU_OPT_INT, 0, NULL},
-{"joy2", "", "1-8 SDL2 joystick index", EMU_OPT_INT, 0, NULL},
-{"kbd", "", "keyboard language DE, GB, US", EMU_OPT_CHAR, 0, "US"},
-{"no_patch", "n", "disable patching the rom", EMU_OPT_INT, 0, NULL},
-{"palette", "", "0 = Full colour, 1 = Unsaturated colours (slightly more CRT like), 2 =  Enable grayscale display", EMU_OPT_INT, 0, NULL},
-{"print", "", "command to use for print jobs", EMU_OPT_CHAR, 0, "lpr"},
-{"ramtop", "r", "The memory space top (128K + QL ram, not valid if ramsize set)", EMU_OPT_INT, 256, NULL},
-{"ramsize", "", "The size of ram", EMU_OPT_INT, 0, NULL},
-{"resolution", "g", "resolution of screen in mode 4", EMU_OPT_CHAR, 0, "512x256"},
-{"romdir", "", "path to the roms", EMU_OPT_CHAR, 0, "roms"},
-{"romport", "", "rom in QL rom port (0xC000 address)", EMU_OPT_CHAR, 0, NULL},
-{"romim", "", "rom in QL rom port (0xC000 address, legacy alias for romport)", EMU_OPT_CHAR, 0, NULL},
-{"ser1", "", "device for ser1", EMU_OPT_CHAR, 0, NULL},
-{"ser2", "", "device for ser2", EMU_OPT_CHAR, 0, NULL},
-{"ser3", "", "device for ser3", EMU_OPT_CHAR, 0, NULL},
-{"ser4", "", "device for ser4", EMU_OPT_CHAR, 0, NULL},
-{"shader", "", "0 = Disabled, 1 = Use flat shader, 2 = Use curved shader", EMU_OPT_INT, 0, NULL},
-{"shader_file", "", "Path to shader file to use if SHADER is 1 or 2", EMU_OPT_CHAR, 0, "shader.glsl"},
-{"skip_boot", "", "1 = skip f1/f2 screen, 0 = show f1/f2 screen", EMU_OPT_INT, 1, NULL},
-{"sound", "", "volume in range 1-8, 0 to disable", EMU_OPT_INT, 0, NULL},
-{"speed", "", "speed in factor of BBQL speed, 0.0 for full speed", EMU_OPT_CHAR, 0, "0.0"},
-{"strict_lock", "", "enable strict file locking", EMU_OPT_INT, 0, NULL},
-{"sysrom", "", "system rom", EMU_OPT_CHAR, 0, "MIN198.rom"},
-{"win_size", "w", "window size 1x, 2x, 3x, max, full", EMU_OPT_CHAR, 0, "1x"},
-{"verbose", "v", "verbosity level 0-3", EMU_OPT_INT, 1, NULL},
+	{ "bdi1", "", "file exposed by the BDI interface", EMU_OPT_CHAR, 0,
+	  NULL },
+	{ "boot_cmd", "b", "command to run on boot (executed in basic)",
+	  EMU_OPT_CHAR, 0, NULL },
+	{ "boot_device", "d", "device to load BOOT file from", EMU_OPT_CHAR, 0,
+	  "mdv1" },
+	{ "cpu_hog", "", "1 = use all cpu, 0 = sleep when idle", EMU_OPT_INT, 1,
+	  NULL },
+	{ "device", "", "QDOS_name,path,flags (may be used multiple times",
+	  EMU_OPT_DEV, 0, NULL },
+	{ "fast_startup", "", "1 = skip ram test (does not affect Minerva)",
+	  EMU_OPT_INT, 0, NULL },
+	{ "filter", "", "enable bilinear filter when zooming", EMU_OPT_INT, 0,
+	  NULL },
+	{ "fixaspect", "",
+	  "0 = 1:1 pixel mapping, 1 = 2:3 non square pixels, 2 = BBQL aspect non square pixels",
+	  EMU_OPT_INT, 0, NULL },
+	{ "iorom1", "", "rom in 1st IO area (Minerva only 0x10000 address)",
+	  EMU_OPT_CHAR, 0, NULL },
+	{ "iorom2", "", "rom in 2nd IO area (Minerva only 0x14000 address)",
+	  EMU_OPT_CHAR, 0, NULL },
+	{ "joy1", "", "1-8 SDL2 joystick index", EMU_OPT_INT, 0, NULL },
+	{ "joy2", "", "1-8 SDL2 joystick index", EMU_OPT_INT, 0, NULL },
+	{ "kbd", "", "keyboard language DE, GB, US", EMU_OPT_CHAR, 0, "US" },
+	{ "no_patch", "n", "disable patching the rom", EMU_OPT_INT, 0, NULL },
+	{ "palette", "",
+	  "0 = Full colour, 1 = Unsaturated colours (slightly more CRT like), 2 =  Enable grayscale display",
+	  EMU_OPT_INT, 0, NULL },
+	{ "print", "", "command to use for print jobs", EMU_OPT_CHAR, 0,
+	  "lpr" },
+	{ "ramtop", "r",
+	  "The memory space top (128K + QL ram, not valid if ramsize set)",
+	  EMU_OPT_INT, 256, NULL },
+	{ "ramsize", "", "The size of ram", EMU_OPT_INT, 0, NULL },
+	{ "resolution", "g", "resolution of screen in mode 4", EMU_OPT_CHAR, 0,
+	  "512x256" },
+	{ "romdir", "", "path to the roms", EMU_OPT_CHAR, 0, "roms" },
+	{ "romport", "", "rom in QL rom port (0xC000 address)", EMU_OPT_CHAR, 0,
+	  NULL },
+	{ "romim", "",
+	  "rom in QL rom port (0xC000 address, legacy alias for romport)",
+	  EMU_OPT_CHAR, 0, NULL },
+	{ "ser1", "", "device for ser1", EMU_OPT_CHAR, 0, NULL },
+	{ "ser2", "", "device for ser2", EMU_OPT_CHAR, 0, NULL },
+	{ "ser3", "", "device for ser3", EMU_OPT_CHAR, 0, NULL },
+	{ "ser4", "", "device for ser4", EMU_OPT_CHAR, 0, NULL },
+	{ "shader", "",
+	  "0 = Disabled, 1 = Use flat shader, 2 = Use curved shader",
+	  EMU_OPT_INT, 0, NULL },
+	{ "shader_file", "", "Path to shader file to use if SHADER is 1 or 2",
+	  EMU_OPT_CHAR, 0, "shader.glsl" },
+	{ "skip_boot", "", "1 = skip f1/f2 screen, 0 = show f1/f2 screen",
+	  EMU_OPT_INT, 1, NULL },
+	{ "sound", "", "volume in range 1-8, 0 to disable", EMU_OPT_INT, 0,
+	  NULL },
+	{ "speed", "", "speed in factor of BBQL speed, 0.0 for full speed",
+	  EMU_OPT_CHAR, 0, "0.0" },
+	{ "strict_lock", "", "enable strict file locking", EMU_OPT_INT, 0,
+	  NULL },
+	{ "sysrom", "", "system rom", EMU_OPT_CHAR, 0, "MIN198.rom" },
+	{ "win_size", "w", "window size 1x, 2x, 3x, max, full", EMU_OPT_CHAR, 0,
+	  "1x" },
+	{ "verbose", "v", "verbosity level 0-3", EMU_OPT_INT, 1, NULL },
 #endif //SQLUX_EMU
 
 #ifdef QLAY_EMU
-{"ramsize", "m", "amount of ram in K (max 8192)", EMU_OPT_INT, 128, NULL, NULL},
-{"exprom", "c", "address@romfile eg C000@NFA.rom", EMU_OPT_DEV, 0, NULL, NULL},
-{"sysrom", "r", "system rom", EMU_OPT_CHAR, 0, "JS.rom", NULL},
-{"drive", "l", "nfa file (upto 8 times)", EMU_OPT_DEV, 0 ,NULL, NULL},
-{"trace", "", "enable tracing", EMU_OPT_INT, 0, NULL, NULL},
-{"trace-map", "", "map file for trace addrs to symbols", EMU_OPT_CHAR, 0, NULL, NULL},
+	{ "drive", "l", "nfa file (upto 8 times)", EMU_OPT_DEV, 0, NULL, NULL },
+	{ "exprom", "c", "address@romfile eg C000@NFA.rom", EMU_OPT_DEV, 0,
+	  NULL, NULL },
+	{ "loglevel", "", "log level 0-5 (5 silent)", EMU_OPT_INT, LOG_ERROR,
+	  NULL, NULL },
+	{ "ramsize", "m", "amount of ram in K (max 8192)", EMU_OPT_INT, 128,
+	  NULL, NULL },
+	{ "sysrom", "r", "system rom", EMU_OPT_CHAR, 0, "JS.rom", NULL },
+	{ "trace", "", "enable tracing", EMU_OPT_INT, 0, NULL, NULL },
+	{ "trace-map", "", "map file for trace addrs to symbols", EMU_OPT_CHAR,
+	  0, NULL, NULL },
 #endif //QLAY_EMU
 
 #ifdef Q68_EMU
-{"smsqe", "", "smsqe image to load (at 0x32000)", EMU_OPT_CHAR, 0, NULL, NULL},
-{"sd1", "", "SDHC Image for SD1 slot", EMU_OPT_CHAR, 0, NULL, NULL},
-{"sysrom", "r", "system rom to load (at 0x0)", EMU_OPT_CHAR, 0, NULL, NULL},
-{"trace", "", "enable tracing", EMU_OPT_INT, 0, NULL, NULL},
+	{ "smsqe", "", "smsqe image to load (at 0x32000)", EMU_OPT_CHAR, 0,
+	  NULL, NULL },
+	{ "sd1", "", "SDHC Image for SD1 slot", EMU_OPT_CHAR, 0, NULL, NULL },
+	{ "sysrom", "r", "system rom to load (at 0x0)", EMU_OPT_CHAR, 0, NULL,
+	  NULL },
+	{ "trace", "", "enable tracing", EMU_OPT_INT, 0, NULL, NULL },
 
 #endif
 
-{NULL, NULL, NULL, 0, 0, NULL, NULL},
+	{ NULL, NULL, NULL, 0, 0, NULL, NULL },
 };
 
-static ArgParser* parser;
+static ArgParser *parser;
 
 static bool match(const char *name, const char *option)
 {
 	return (strcasecmp(name, option) == 0);
 }
 
-static int iniHandler(__attribute__ ((unused)) void* user, __attribute__ ((unused)) const char* section, const char* name,
-                   const char* value)
+static int iniHandler(__attribute__((unused)) void *user,
+		      __attribute__((unused)) const char *section,
+		      const char *name, const char *value)
 {
 	int i;
 
@@ -191,21 +226,24 @@ int emulatorOptionParse(int argc, char **argv)
 		UT_string *helpItem;
 		utstring_new(helpItem);
 
-		if(strlen(emuOptions[i].alias)) {
-			utstring_printf(helpItem, "  -%s,", emuOptions[i].alias);
+		if (strlen(emuOptions[i].alias)) {
+			utstring_printf(helpItem, "  -%s,",
+					emuOptions[i].alias);
 		} else {
 			utstring_printf(helpItem, "  ");
 		}
 		utstring_printf(helpItem, "--%s", emuOptions[i].option);
 
 		if (emuOptions[i].type == EMU_OPT_INT) {
-			utstring_printf(helpItem, " [%d]", emuOptions[i].intVal);
+			utstring_printf(helpItem, " [%d]",
+					emuOptions[i].intVal);
 		} else if (emuOptions[i].type == EMU_OPT_CHAR) {
 			if (emuOptions[i].charVal != NULL) {
-				utstring_printf(helpItem, " [%s]", emuOptions[i].charVal);
+				utstring_printf(helpItem, " [%s]",
+						emuOptions[i].charVal);
 			}
 		}
-		for (j = utstring_len(helpItem); j < 30; j++){
+		for (j = utstring_len(helpItem); j < 30; j++) {
 			utstring_printf(helpItem, " ");
 		}
 
@@ -232,9 +270,8 @@ int emulatorOptionParse(int argc, char **argv)
 		utstring_new(optItem);
 
 		if (strlen(emuOptions[i].alias)) {
-			utstring_printf(optItem, "%s %s",
-				emuOptions[i].option,
-				emuOptions[i].alias);
+			utstring_printf(optItem, "%s %s", emuOptions[i].option,
+					emuOptions[i].alias);
 		} else {
 			utstring_printf(optItem, "%s", emuOptions[i].option);
 		}
@@ -279,9 +316,9 @@ const char *emulatorOptionString(const char *name)
 
 	i = 0;
 	while (emuOptions[i].option != NULL) {
-		if ((strcmp(emuOptions[i].option, name) == 0)
-			&& (emuOptions[i].type == EMU_OPT_CHAR)
-			&& (emuOptions[i].charVal != NULL)) {
+		if ((strcmp(emuOptions[i].option, name) == 0) &&
+		    (emuOptions[i].type == EMU_OPT_CHAR) &&
+		    (emuOptions[i].charVal != NULL)) {
 			return emuOptions[i].charVal;
 		}
 		i++;
@@ -309,7 +346,8 @@ int emulatorOptionInt(const char *name)
 	return 0;
 }
 
-int emulatorOptionDevCount(const char *name) {
+int emulatorOptionDevCount(const char *name)
+{
 	int i, count;
 
 	count = ap_count(parser, name);
@@ -346,7 +384,8 @@ const char *emulatorOptionDev(const char *name, int idx)
 			if (emuOptions[i].list != NULL) {
 				int cur = 0;
 				struct emuList *entry;
-				LL_FOREACH(emuOptions[i].list, entry) {
+				LL_FOREACH(emuOptions[i].list, entry)
+				{
 					if (idx == cur) {
 						return entry->option;
 					}
