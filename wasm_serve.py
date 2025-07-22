fbc32e6f28170e5a47ba4eeb74edd4729300ce75
Graeme Gregory <graeme@xora.org.uk>
2025-07-22 19:57:07 +0100

    wasm_server.py : launching browser was annoying
    Signed-off-by: Graeme Gregory <graeme@xora.org.uk>

--- wasm_serve.py
+++ wasm_serve.py
@@ -23,8 +23,7 @@ if __name__ == "__main__":
     httpd = server(port)
     try:
         os.chdir('wasm')
-        print("\nserving from build/ at localhost:" + str(port))
-        webbrowser.open("http://localhost:8666/q68.html",new=2)
+        print("\nserving from wasm/ at localhost:" + str(port))
         httpd.serve_forever()
     except KeyboardInterrupt:
         print("\n...shutting down http server")
