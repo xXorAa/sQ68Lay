ed97b4fc505c8bb059cded0a16a1c95ec99bab15
Graeme Gregory <graeme@xora.org.uk>
2025-07-23 20:24:59 +0100

    qlay_sound.c, qlay_io.c : add MDV sound
    Play a sound when MDV motor is on!
    
    Sound sample from :-
    
    https://www.lingula.org.uk/~steve/share/vdrive-sound/mdvsnd-attiny85.c
    
    Signed-off-by: Graeme Gregory <graeme@xora.org.uk>

---
+++ include/qlay_sound.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2020-2022 Graeme Gregory
+ *
+ * SPDX: GPL-2.0-only
+ */
+
+#pragma once
+
+#ifndef QLAY_SOUND_H
+#define QLAY_SOUND_H
+
+bool qlayInitMdvSound(void);
+bool qlayStartMdvSound(void);
+bool qlayStopMdvSound(void);
+
+#endif /* QLAY_SOUND_H */
