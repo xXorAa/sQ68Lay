c8bc84f5acbe092cf3188faa69610876cc1b2135
Graeme Gregory <graeme@xora.org.uk>
2025-08-04 16:42:41 +0100

    qlay_sound : convert to QLAY_LOG_SOUND
    Using custom log levels means they can be selected seperately in
    debugging exercises.

---
+++ include/emulator_logging.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2022 Graeme Gregory
+ *
+ * SPDX: GPL-2.0-only
+ */
+#pragma once
+
+#ifndef EMULATOR_LOGGING
+#define EMULATOR_LOGGING
+
+#include <SDL3/SDL.h>
+
+enum {
+	QLAY_LOG_SOUND = SDL_LOG_CATEGORY_CUSTOM,
+};
+
+#endif //EMULATOR_LOGGIN
